b0e2b92d25a3b837aa3dbf1e05e401cdb9806c17
Paulo Leão <ptalregister@gmail.com>
2025-08-26 17:55:47 -0300

    Organizing folders

---
+++ ibmec-ds-lessons/0-getting-started/3-comments/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (void)
+{
+    printf("Hello, "); //This is a line comment. 
+    
+    printf("World!\n"); /*This is a block comment*/
+    
+    return 0;
+}
---
+++ ibmec-ds-lessons/0-getting-started/4-whitespace/main.c
@@ -0,0 +1,20 @@
+
+/*
+It's possible to write programs in C in different ways. 
+What defines their operation has nothing to do with correct indentation, 
+but rather with the command lines. Therefore, different writing styles are accepted by the computer. 
+Acceptance doesn't mean correct or a good practice.
+*/
+
+//Exemple one 
+/* 
+#include <stdio.h> int main () { printf("Hello, World!\n"); return 0; }
+*/
+
+//Exemple two
+/* 
+#include <stdio.h>                            int main ()
+ { printf("Hello, World!\n");
+  
+                                                return 0; }
+*/
