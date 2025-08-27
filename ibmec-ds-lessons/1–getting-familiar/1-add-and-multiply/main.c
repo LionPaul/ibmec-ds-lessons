774d94dd80bddc43e38f976aa5cfb3673835cc48
Paulo Leão <ptalregister@gmail.com>
2025-08-27 19:11:57 -0300

    Update

---
+++ ibmec-ds-lessons/1–getting-familiar/1-add-and-multiply/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+    int num1, num2, ans;
+
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+
+    ans = (num1 + num2) * 3;
+
+    print("%d", ans);
+    return 0;
+}
